1e41700dd999fee22ffeabfecc27a7b1ec70f255
vinay <vinaypanmand@gmail.com>
2025-07-19 16:46:34 +0530

    test2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,6 +6,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
+//khhgjhjv
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
